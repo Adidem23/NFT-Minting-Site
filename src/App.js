aa300d31ec104f96391b25d95dba54e80a8b13df
Aditya Suryawanshi <adityasuryawanshi5451@gmail.com>
2023-04-26 01:12:03 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,81 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
+import { useState } from "react";
 import "./styles/Home.css";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import { ThirdwebSDKProvider } from "@thirdweb-dev/react";
+import { BigNumber } from "ethers";
+import { ethers } from 'ethers';
+
+
 
 export default function Home() {
+
+  const [list, setlist] = useState([]);
+
+  const getcontract = async () => {
+    console.log("Clicked");
+    const sdk = new ThirdwebSDK("Mumbai");
+    const contract = await sdk.getContract("0x0C26c22F95752fE46EDDf0caa11e7Aa984238D1b", "marketplace");
+    const listings = await contract.getActiveListings();
+    setlist(listings);
+    const priceOfFirstActiveListing = listings[0].price;
+    console.log(listings[0].asset.name);
+  }
+
+  console.log(list);
+
   return (
-    <div className="container">
-      <main className="main">
-        <h1 className="title">
-          Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
-        </h1>
 
-        <p className="description">
-          Get started by configuring your desired network in{" "}
-          <code className="code">src/index.js</code>, then modify the{" "}
-          <code className="code">src/App.js</code> file!
-        </p>
+    <>
+      <ThirdwebSDKProvider
+        activeChain={"Mumbai"}
+        signer={new ethers.providers.Web3Provider(window.ethereum).getSigner()}
+      >
 
-        <div className="connect">
-          <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center'}} />
+        <div style={{ display: "block", margin: 'auto', width: "fit-content" }}>
+          <ConnectWallet />
+          <h2>Hello</h2>
+          <button onClick={getcontract}>Show All NFTS</button>
+          <hr />
+          <div id="nftsdiv">
+            {
+              list && list.map((nft) => {
+                return (
+                  <>
+                    <div style={{ display: "block", margin: 'auto', width: "fit-content", border: '3px solid red', marginTop: "30px" }}>
+
+                      <img src={nft.asset.image} width="600px" height="250px" />
+                      <h2>NFT Name: {nft.asset.name}</h2>
+                      <h3>Price: {nft.buyoutCurrencyValuePerToken.displayValue} &nbsp; {nft.buyoutCurrencyValuePerToken.name}</h3>
+                      <button onClick={async () => {
+                        try {
+
+                          const sdk = new ThirdwebSDK("Mumbai");
+                          const contract = await sdk.getContract("0x0C26c22F95752fE46EDDf0caa11e7Aa984238D1b", "marketplace");
+
+                          const quantityDesired = 1;
+
+                          await contract.buyoutListing(BigNumber.from(nft.id, 1), quantityDesired);
+
+                        } catch (err) {
+                          alert("Error Has been Occured" + err);
+                          console.log(err);
+                        }
+
+                      }}> BUY NOW </button>
+                    </div>
+                  </>
+                )
+              })
+            }
+          </div>
         </div>
+      </ThirdwebSDKProvider>
 
-        <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
 
-          <a href="https://thirdweb.com/dashboard" className="card">
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
 
-          <a href="https://portal.thirdweb.com/templates" className="card">
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
+    </>
+
+
   );
 }
